82205bf2005b471e1b4bee69dad1732b68b86564
jolah1 <jokunlade@gmail.com>
2022-06-10 09:47:49 -0700

    added get builtin

--- exit_shell.c
+++ exit_shell.c
@@ -3,7 +3,7 @@
 /**
  * exit_shell - exits the shell
  * @datash: data relevant (status and args)
- * Return: 0 on success.
+ * Return: always return success
  */
 
 int exit_shell(data_shell *datash)
