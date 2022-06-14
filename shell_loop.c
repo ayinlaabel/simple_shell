8878854195f5227835a91869954b60fe4b3ea567
Ayinla Abel <kraneabel@gmail.com>
2022-06-14 08:27:47 -0700

    shell_loop.c updated

--- shell_loop.c
+++ shell_loop.c
@@ -5,6 +5,7 @@
  * @in: input string
  * Return: input without comments
  */
+
 char *without_comment(char *in)
 {
 	int i, up_to;
@@ -39,16 +40,19 @@ char *without_comment(char *in)
  * @datash: data relevant (av, input, args)
  * Return: no return.
  */
+
 void shell_loop(data_shell *datash)
 {
 	int loop, i_eof;
 	char *input;
 
 	loop = 1;
+
 	while (loop == 1)
 	{
 		write(STDIN_FILENO, "^-^ ", 4);
 		input = read_line(&i_eof);
+
 		if (i_eof != -1)
 		{
 			input = without_comment(input);
