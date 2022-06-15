6f7110f4f9f675b8a5fd0542723cf75f5cd809d3
Ayinla Abel <kraneabel@gmail.com>
2022-06-15 06:18:46 -0700

    cmd_exec.c updated

--- cmd_exec.c
+++ cmd_exec.c
@@ -11,7 +11,7 @@ int is_cdir(char *path, int *i)
 {
 	if (path[*i] == ':')
 		return (1);
-	
+
 	while (path[*i] != ':' && path[*i])
 	{
 		*i += 1;
@@ -37,41 +37,35 @@ char *_which(char *cmd, char **_environ)
 	struct stat st;
 
 	path = _getenv("PATH", _environ);
-
 	if (path)
 	{
 		ptr_path = _strdup(path);
 		len_cmd = _strlen(cmd);
 		token_path = _strtok(ptr_path, ":");
 		i = 0;
-
-																			while (token_path != NULL)
+		while (token_path != NULL)
 		{
 			if (is_cdir(path, &i))
 				if (stat(cmd, &st) == 0)
 					return (cmd);
 
 			len_dir = _strlen(token_path);
-			
 			dir = malloc(len_dir + len_cmd + 2);
 			_strcpy(dir, token_path);
-
 			_strcat(dir, "/");
 			_strcat(dir, cmd);
 			_strcat(dir, "\0");
-
 			if (stat(dir, &st) == 0)
 			{
 				free(ptr_path);
 				return (dir);
 			}
 			free(dir);
-
 			token_path = _strtok(NULL, ":");
 		}
-																			free(ptr_path);
-																			if (stat(cmd, &st) == 0)
-																				return (cmd);
+		free(ptr_path);
+		if (stat(cmd, &st) == 0)
+			return (cmd);
 		return (NULL);
 	}
 	if (cmd[0] == '/')
@@ -184,14 +178,12 @@ int cmd_exec(data_shell *datash)
 	exec = is_executable(datash);
 	if (exec == -1)
 		return (1);
-	
 	if (exec == 0)
 	{
 		dir = _which(datash->args[0], datash->_environ);
 		if (check_error_cmd(dir, datash) == 1)
 			return (1);
 	}
-	
 	pd = fork();
 	if (pd == 0)
 	{
@@ -213,8 +205,6 @@ int cmd_exec(data_shell *datash)
 			wpd = waitpid(pd, &state, WUNTRACED);
 		} while (!WIFEXITED(state) && !WIFSIGNALED(state));
 	}
-
 	datash->status = state / 256;
-
 	return (1);
 }
